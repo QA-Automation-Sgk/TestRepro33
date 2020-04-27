6a9847065ca368e46af23f7157d931eb240e81f0
papa.danso <papa.danso@CHEN2-QA-AM02.asia.schawk.com>
2020-04-27 17:22:03 +0530

    test66t

---
+++ Test Suites/NewTestSuite22.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NewTestSuite22</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>620250eb-ca3f-4ce3-91e1-6b6455f6530e</testSuiteGuid>
+   <testCaseLink>
+      <guid>0acfba1c-bfe7-4904-99ea-802eb70d7d07</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginEncore</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
